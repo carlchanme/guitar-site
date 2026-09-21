5bdf9531ef6ce86ba991388fae22d90de719eb43
Carl Chan <carlking1st@yahoo.com>
2026-09-21 17:13:04 +0800

    Domain: dork.my as site URL, sitemap, robots, canonical and OG tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://dork.my',
+  integrations: [sitemap()],
   redirects: {
     '/drills': '/day/',
     '/drills/week-01': '/day/1/',
